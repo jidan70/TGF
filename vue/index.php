93f9066260b544e5996a29677439811356640680
jidan70 <jidan70@hotmail.fr>
2015-02-04 19:47:49 +0100

    division du projet en modele mvc, debut de la page index.php, ajout de la page mixtapes.php, ajout des images logo, ajout fichier css, bootstrap

---
+++ vue/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Team Gone Fly</title>
+  <link rel="stylesheet" href="css\bootstrap_cyborg.min.css">
+  <link rel="stylesheet" href="css/tgf.css"
+
+  <style>
+  </style>
+
+</head>
+<body>
+  <div>
+
+    <?php include('template/header.php') ?>
+
+    <div class="col-md-10 col-md-offset-1" style="border-radius:5px;margin-top:80px;">
+      <div class="col-md-10 col-md-offset-1">
+        <div class="media embed-responsive embed-responsive-16by9">
+          <iframe width="854" height="510" src="https://www.youtube.com/embed/MAbTMslDKCM" frameborder="0" allowfullscreen></iframe>
+        </div>
+        <div>
+          <div align="center"><embed src="http://www.datpiff.com/embed/mixtape/mc70b953/" quality="high" wmode="direct" pluginspage="http://www.adobe.com/shockwave/download/download.cgi?P1_Prod_Version=ShockwaveFlash" type="application/x-shockwave-flash" width="507" height="221" allowScriptAccess="always" allowScripting="on"><br><a href="http://www.datpiff.com/DaddyO-Trey-P-FBG-Duck-Boss-Ko-2-Storys-mixtape.681345.html" target="_blank">Download Mixtape</a> | <a href="http://www.datpiff.com" target="_blank">Free Mixtapes</a> Powered by <a href="http://www.datpiff.com/" target="_blank" title="Free Mixtapes">DatPiff.com</a></div>
+        </div>
+      </div>
+    </div>
+
+  </div>
+
+</body>
+</html>
---
+++ vue/mixtapes.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Team Gone Fly</title>
+  <link rel="stylesheet" href="css\bootstrap_cyborg.min.css">
+  <link rel="stylesheet" href="css/tgf.css"
+
+  <style>
+  </style>
+
+</head>
+<body>
+  <div>
+
+    <?php include('template/header.php') ?>
+
+
+    <div class="col-md-10 col-md-offset-1" style="border:#4682B4 solid 1px;;border-radius:5px;margin-top:80px;">
+
+    </div>
+
+  </div>
+
+
+</body>
+</html>
