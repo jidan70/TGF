05141f4fb70f8d113954a253928a00f11c8b9023
jidan70 <jidan70@hotmail.fr>
2015-02-05 02:32:02 +0100

    modification largeur des embed dans mixtapes.php, rectification href des import bootstrap

--- vue/mixtapes.php
+++ vue/mixtapes.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <title>Team Gone Fly</title>
-  <link rel="stylesheet" href="bootstrap\dist\css\bootstrap_cyborg.min.css">
+  <link rel="stylesheet" href="bootstrap/dist/css/bootstrap_cyborg.min.css">
   <link rel="stylesheet" href="css/tgf.css">
   <link rel="stylesheet" href="css/animate.css">
 
@@ -20,27 +20,27 @@
     <div class="col-md-10 col-md-offset-1" style="border:#4682B4 solid 4px;;border-radius:5px;margin-top:80px;padding-top:15px;">
       <div>
         <div class="col-md-4">
-          <div align="center"><embed src="http://www.datpiff.com/embed/mixtape/mc70b953/" quality="high" wmode="direct" pluginspage="http://www.adobe.com/shockwave/download/download.cgi?P1_Prod_Version=ShockwaveFlash" type="application/x-shockwave-flash" width="507" height="221" allowScriptAccess="always" allowScripting="on"><br>
+          <div align="center"><embed style="border-radius:10px;" src="http://www.datpiff.com/embed/mixtape/mc70b953/" quality="high" wmode="direct" pluginspage="http://www.adobe.com/shockwave/download/download.cgi?P1_Prod_Version=ShockwaveFlash" type="application/x-shockwave-flash" width="100%" height="221" allowScriptAccess="always" allowScripting="on"><br>
             <a class="btn btn-primary download" style="margin-bottom:10px;" href="http://www.datpiff.com/pop-mixtape-download.php?id=mc70b953" target="_blank">Download Mixtape</a>
           </div>
         </div>
         <div class="col-md-4">
-          <div align="center"><embed src="http://www.datpiff.com/embed/mixtape/m0ae63b5/" quality="high" wmode="direct" pluginspage="http://www.adobe.com/shockwave/download/download.cgi?P1_Prod_Version=ShockwaveFlash" type="application/x-shockwave-flash" width="507" height="221" allowScriptAccess="always" allowScripting="on"><br>
+          <div align="center"><embed src="http://www.datpiff.com/embed/mixtape/m0ae63b5/" quality="high" wmode="direct" pluginspage="http://www.adobe.com/shockwave/download/download.cgi?P1_Prod_Version=ShockwaveFlash" type="application/x-shockwave-flash" width="100%" height="221" allowScriptAccess="always" allowScripting="on"><br>
             <a class="btn btn-primary download" style="margin-bottom:10px;" href="http://www.datpiff.com/pop-mixtape-download.php?id=m0ae63b5" target="_blank">Download Mixtape</a>
             </div>
         </div>
         <div class="col-md-4">
-          <div align="center"><embed src="http://www.datpiff.com/embed/mixtape/m871c88b/" quality="high" wmode="direct" pluginspage="http://www.adobe.com/shockwave/download/download.cgi?P1_Prod_Version=ShockwaveFlash" type="application/x-shockwave-flash" width="507" height="221" allowScriptAccess="always" allowScripting="on"><br>
+          <div align="center"><embed src="http://www.datpiff.com/embed/mixtape/m871c88b/" quality="high" wmode="direct" pluginspage="http://www.adobe.com/shockwave/download/download.cgi?P1_Prod_Version=ShockwaveFlash" type="application/x-shockwave-flash" width="100%" height="221" allowScriptAccess="always" allowScripting="on"><br>
             <a class="btn btn-primary download" style="margin-bottom:10px;" href="http://www.datpiff.com/pop-mixtape-download.php?id=m871c88b" target="_blank">Download Mixtape</a>
           </div>
         </div>
         <div class="col-md-4">
-          <div align="center"><embed src="http://www.datpiff.com/embed/mixtape/ma203a03/" quality="high" wmode="direct" pluginspage="http://www.adobe.com/shockwave/download/download.cgi?P1_Prod_Version=ShockwaveFlash" type="application/x-shockwave-flash" width="507" height="221" allowScriptAccess="always" allowScripting="on"><br>
+          <div align="center"><embed src="http://www.datpiff.com/embed/mixtape/ma203a03/" quality="high" wmode="direct" pluginspage="http://www.adobe.com/shockwave/download/download.cgi?P1_Prod_Version=ShockwaveFlash" type="application/x-shockwave-flash" width="100%" height="221" allowScriptAccess="always" allowScripting="on"><br>
             <a class="btn btn-primary download" style="margin-bottom:10px;" href="http://www.datpiff.com/pop-mixtape-download.php?id=ma203a03" target="_blank">Download Mixtape</a>
           </div>
         </div>
         <div class="col-md-4">
-          <div align="center"><embed src="http://www.datpiff.com/embed/mixtape/m441d1fa/" quality="high" wmode="direct" pluginspage="http://www.adobe.com/shockwave/download/download.cgi?P1_Prod_Version=ShockwaveFlash" type="application/x-shockwave-flash" width="507" height="221" allowScriptAccess="always" allowScripting="on"><br>
+          <div align="center"><embed src="http://www.datpiff.com/embed/mixtape/m441d1fa/" quality="high" wmode="direct" pluginspage="http://www.adobe.com/shockwave/download/download.cgi?P1_Prod_Version=ShockwaveFlash" type="application/x-shockwave-flash" width="100%" height="221" allowScriptAccess="always" allowScripting="on"><br>
             <a class="btn btn-primary download" style="margin-bottom:10px;" href="http://www.datpiff.com/pop-mixtape-download.php?id=m441d1fa" target="_blank">Download Mixtape</a>
           </div>
         </div>
--- vue/template/script.php
+++ vue/template/script.php
@@ -1,3 +1,3 @@
 <script src="js/jquery.js"></script>
 <script src="js/animate.js"></script>
-<script src="bootstrap\dist\js\bootstrap.min.js"></script>
+<script src="bootstrap/dist/js/bootstrap.min.js"></script>
