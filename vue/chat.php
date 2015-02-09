332b9a072de6560e345aa44850aa70ab4652a1a0
jidan70 <jidan70@hotmail.fr>
2015-02-09 22:47:21 +0100

    ajout de smiley et integraion js pour selectionner les smiley, ajout fenetre modale pour les smiley

--- vue/chat.php
+++ vue/chat.php
@@ -19,43 +19,44 @@ include_once('../modele/connexion_bdd.php');
 
 </head>
 <body>
+  <?php include('template/smiley.php') ?>
+  <div id="ar-fenetre-modale"></div>
 
 
   <div>
 
     <?php include('template/header.php') ?>
 
-    <div class="chat col-md-10 col-md-offset-1" style="margin-top:50px;">
 
-      <fieldset class="col-md-4 col-md-offset-4" style="border:#2a9fd6 solid 3px;border-radius:4px;background:rgba(136,136,136,0.9);padding:10px;">
-        <form action="../controleur/envoi_message.php" method="post">
-          <p class="text-center"><textarea class="form-control" name="message" id="message" placeholder="enter your message here"></textarea><p>
-          <input type="submit" class="btn btn-primary pull-right" value="send">
-        </form>
-      </fieldset>
+      <div id="chat-section" class="chat-section col-md-10 col-md-offset-1" style="border:#2a9fd6 solid 2px;border-radius:4px;background:rgba(136,136,136,0); margin-top:40px;padding-bottom:40px;">
+          <h1 class="text-center font-global">Chiraq</h1>
+          <hr style="margin-bottom:30px;"/>
+        <div class="col-md-5 col-md-offset-1" style="height:700px;overflow:scroll;border:#2a9fd6 solid 1px;border-radius:5px;">
+          <?php
+          $messages = $tgf_base->query('SELECT message from chat ORDER BY date_creation DESC');
 
-    </div>
-
-      <div id="chat-section" class="chat-section col-md-10 col-md-offset-1" style="border:#2a9fd6 solid 2px;border-radius:4px;background:rgba(136,136,136,0.2); margin-top:40px;">
-        <h1 class="text-center font-global">Chiraq</h1>
-        <hr style="margin-bottom:30px;"/>
-        <?php
-        $messages = $tgf_base->query('SELECT message from chat ORDER BY date_creation DESC');
-
-        while($message = $messages->fetch())
-        {
-        ?>
-        <div class="row">
-          <div class="col-md-4 col-md-offset-1" style="margin-top:20px;border:solid 1px;border-radius:5px;background:#fff;">
-            <p style="margin-top:10px;color:#000;"><?php echo nl2br(htmlspecialchars($message['message'])); ?></p>
+          while($message = $messages->fetch())
+          {
+          ?>
+          <div class="row">
+            <div class="col-md-12 col-md-offset-0" style="margin-top:20px;margin-left:10px;margin-right:10px;border:solid 1px;border-radius:5px;background:#fff;">
+              <p style="margin-top:10px;color:#000;"><?php echo nl2br(htmlspecialchars($message['message'])); ?></p>
+            </div>
           </div>
+          <?php
+          }
+
+          $messages->closeCursor();
+          ?>
         </div>
-        <?php
-        }
 
-        $messages->closeCursor();
-        ?>
-
+        <fieldset class="col-md-4 col-md-offset-1" style="border:#2a9fd6 solid 3px;border-radius:4px;background:rgba(136,136,136,0.9);padding:10px;">
+          <form id="form-chat" action="../controleur/envoi_message.php" method="post">
+            <p class="text-center"><textarea class="form-control" id="enter_champ" name="message" id="message" placeholder="enter your message here"></textarea><p>
+            <button id="smiley" class="btn btn-default">SMILEY</button>
+            <input type="submit" class="btn btn-primary pull-right" value="send">
+          </form>
+        </fieldset>
       </div>
 
       <?php include('template/footer.php') ?>
@@ -63,7 +64,45 @@ include_once('../modele/connexion_bdd.php');
   </div>
 
   <?php include('template/script.php') ?>
+  <script>
+
+  var bouton_smiley = document.getElementById('smiley');
+  var tableau_smiley = document.getElementById('tableau-smiley');
+  var form_chat = document.getElementById('form-chat');
+  var fond_modal = document.getElementById('ar-fenetre-modale');
+  var smileys = document.querySelectorAll('#tableau-smiley img');
+  var enter_champ = document.getElementById('enter_champ');
+  var nbr_sml = smileys.length;
+
+  bouton_smiley.addEventListener('click', function(e) {
+
+    e.preventDefault();
+    if (tableau_smiley.style.display == "none" ){
+      tableau_smiley.style.display = "block";
+      fond_modal.style.display = "block";
+    }
+    else{
+      tableau_smiley.style.display = "none";
+      fond_modal.style.display = "none";
+    }
+
+  }, false);
+
+  fond_modal.addEventListener('click', function(e) {
+    tableau_smiley.style.display = "none";
+    fond_modal.style.display = "none";
+  }, false);
+
+  for(var i = 0; i < nbr_sml; i++){
+    smileys[i].addEventListener('click',function(e) {
+      var text_integr = "(" + e.target.id + ")";
+      enter_champ.value += text_integr;
+    }, false);
+  }
 
 
+
+
+  </script>
 </body>
 </html>
---
+++ vue/template/smiley.php
@@ -0,0 +1,44 @@
+<div id="tableau-smiley" class="row">
+<h1 class="font-global" style="color:#000;">Click on the smiley</h1>
+<hr/>
+  <div class="row">
+<div class="col-md-1">
+    <img src="smiley1/angel.png" id="angel"/>
+</div>
+<div class="col-md-1">
+    <img src="smiley1/awww.png" id="awww"/>
+</div>
+<div class="col-md-1">
+    <img src="smiley1/disheartened.png" id="disheartened"/>
+</div>
+<div class="col-md-1">
+    <img src="smiley1/ecstatic.png" id="ecstatic"/>
+</div>
+<div class="col-md-1">
+    <img src="smiley1/great.png" id="great"/>
+</div>
+<div class="col-md-1">
+    <img src="smiley1/i-think.png" id="i-think"/>
+</div>
+<div class="col-md-1">
+    <img src="smiley1/just-like-that.png" id="just-like-that"/>
+</div>
+<div class="col-md-1">
+    <img src="smiley1/kill-u.png" id="kill-u"/>
+</div>
+<div class="col-md-1">
+    <img src="smiley1/mouthshut.png" id="mouthshut"/>
+</div>
+<div class="col-md-1">
+    <img src="smiley1/nice.png" id="nice"/>
+</div>
+<div class="col-md-1">
+    <img src="smiley1/omg.png" id="omg"/>
+</div>
+<div class="col-md-1">
+    <img src="smiley1/sad.png" id="sad"/>
+</div>
+
+
+  </div>
+</div>
