d61f1f5a1d14fe703eae190a3b3641876d71ac5b
jidan70 <jidan70@hotmail.fr>
2015-02-14 00:06:25 +0100

    finition du background menu

--- vue/js/animation_bg.js
+++ vue/js/animation_bg.js
@@ -5,20 +5,20 @@ var p1 = 0, p2 = 300, p3= 0;
 
 
 setInterval(function(){
-  menu_nav.style.backgroundPosition = p1 + "px 0%," + p2 + "px  100%," + p3 + "px 10%, 85% 10%";
+  menu_nav.style.backgroundPosition = p1 + "px 0%," + p1 +  "px 125%, 50% 100%, 85% 10%, " + p2 + "px 50% ";
   if(p1 > 1700){
     p1 = 0;
     p3= 600;
   }
-  if(p2 > 1700){
-    p2 = 0;
+  if(p2 > 2000){
+    p2 = -300;
   }
   if(p3 > 1700){
     p3 = 0;
   }
 
   p1 +=0.7;
-  p2 += 2;
+  p2 += 10;
   p3 += 1;
 },50);
 })();
