a4809801e29de11d794d6b52758ccc93cf01738f
jidan70 <jidan70@hotmail.fr>
2015-02-13 21:57:53 +0100

    modification background menu

--- vue/js/animation_bg.js
+++ vue/js/animation_bg.js
@@ -1,15 +1,24 @@
 (function(){
 var menu_nav = document.querySelector('.menu-nav');
 
-var position_left = 0;
+var p1 = 0, p2 = 300, p3= 0;
 
 
 setInterval(function(){
-  menu_nav.style.backgroundPosition = position_left + "px 100%, 85% 10%";
-  if(position_left > 1000){
-    position_left = 0;
+  menu_nav.style.backgroundPosition = p1 + "px 0%," + p2 + "px  100%," + p3 + "px 10%, 85% 10%";
+  if(p1 > 1700){
+    p1 = 0;
+    p3= 600;
+  }
+  if(p2 > 1700){
+    p2 = 0;
+  }
+  if(p3 > 1700){
+    p3 = 0;
   }
 
-  position_left += 1;
-},100);
+  p1 +=0.7;
+  p2 += 2;
+  p3 += 1;
+},50);
 })();
